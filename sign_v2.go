05de8041f74024401208b881359d7d28152ca319
Ray Wang <ewangplay@gmail.com>
2023-04-04 14:02:55 +0800

    Add signDocByKeywordV2 api

--- sign_v2.go
+++ sign_v2.go
@@ -77,3 +77,79 @@ func (c *Client) SignDocByPercentV2(si *PercentSignModels) (signedDocURL string,
 
 	return
 }
+
+type KeywordSignModel struct {
+	// 印章ID
+	SealID string `json:"sealId"`
+	// 盖章关键字
+	Keyword string `json:"keyword"`
+	// 关键字查找范围。0：全部，1：指定页
+	Scope int `json:"scope"`
+	// 关键字查找开始页，scope为1时有效
+	Start int `json:"start"`
+	// 关键字查找结束页，scope为1时有效
+	End int `json:"end"`
+	// 印章缩放比例,取值范围：0-1
+	Zoom float64 `json:"zoom"`
+	// 左右偏移量，正值表示向右偏移，负值表示向左偏移
+	OffsetX float64 `json:"offsetX"`
+	// 上下偏移量，正值表示向下偏移，负值表示向上偏移
+	OffsetY float64 `json:"offsetY"`
+}
+
+// KeywordSignModels represents the keyword signing info to an ess document.
+type KeywordSignModels struct {
+	// 文档ID
+	DocID string `json:"docId"`
+	// 备注
+	Remark string `json:"remark"`
+	// 签署信息
+	Signs []*KeywordSignModel `json:"signs"`
+}
+
+// SignDocByKeywordV2 signs an ess document by specified keywords.
+func (c *Client) SignDocByKeywordV2(si *KeywordSignModels) (signedDocURL string, err error) {
+	url := fmt.Sprintf("http://%s/ess/api/v2/user/doc/sign/keyword", c.opts.Addr)
+
+	data, err := c.postObject(url, si)
+	if err != nil {
+		return
+	}
+
+	v, ok := data.(map[string]interface{})
+	if !ok {
+		err = fmt.Errorf("response data invalid: %v", data)
+		return
+	}
+
+	tv, found := v["id"]
+	if !found {
+		err = fmt.Errorf("response data invalid: %v", v)
+		return
+	}
+
+	docID, ok := tv.(string)
+	if !ok {
+		err = fmt.Errorf("response data invalid: %v", tv)
+		return
+	}
+
+	if docID != si.DocID {
+		err = fmt.Errorf("response data invalid: id is unmatched")
+		return
+	}
+
+	tv, found = v["url"]
+	if !found {
+		err = fmt.Errorf("response data invalid: %v", v)
+		return
+	}
+
+	signedDocURL, ok = tv.(string)
+	if !ok {
+		err = fmt.Errorf("response data invalid: %v", tv)
+		return
+	}
+
+	return
+}
--- sign_v2_test.go
+++ sign_v2_test.go
@@ -18,8 +18,8 @@ func TestSignDocByPercentV2(t *testing.T) {
 	}
 
 	si := &PercentSignModels{
-		DocID:  "53d8c95886b584690186b58c4b820002",
-		Remark: "王晓辉的存证证书盖章",
+		DocID:  "53d8992f8736c311018736d90ee00003",
+		Remark: "OFD文档位置盖章测试",
 		Signs: []*PercentSignModel{
 			{
 				SealID: "ff80808185beb7db0185bebbac9a0006",
@@ -27,7 +27,7 @@ func TestSignDocByPercentV2(t *testing.T) {
 				X:      0.5,
 				Y:      0.6,
 				Zoom:   0.5,
-				Reason: "存证",
+				Reason: "OFD文档位置盖章测试",
 			},
 		},
 	}
@@ -35,6 +35,50 @@ func TestSignDocByPercentV2(t *testing.T) {
 	signedDocURL, err := c.SignDocByPercentV2(si)
 	if err != nil {
 		t.Fatalf("SignDocByPercentV2 failed: %v", err)
+	}
+
+	fmt.Println("签名文档路径: ", signedDocURL)
+}
+
+func TestSignDocByKeywordV2(t *testing.T) {
+	// 新建客户端
+	opts := &Options{
+		AppKey:    APP_KEY_TEST,
+		AppSecret: APP_SECRET_TEST,
+		Addr:      ADDR_TEST,
+	}
+	c, err := NewClient(opts)
+	if err != nil {
+		t.Fatalf("NewClient failed: %v", err)
+	}
+
+	si := &KeywordSignModels{
+		DocID:  "53d8992f874b903f01874b9e8b470001",
+		Remark: "OFD文档关键字盖章测试",
+		Signs: []*KeywordSignModel{
+			{
+				SealID:  "db09f65231610d7a0002",
+				Keyword: "长恨歌",
+				Zoom:    0.8,
+				OffsetX: 10,
+			},
+			{
+				SealID:  "db09f65231610d7a0002",
+				Keyword: "长歌行",
+				Zoom:    0.5,
+				OffsetY: 5,
+			},
+			{
+				SealID:  "db09f65231610d7a0002",
+				Keyword: "警世贤文",
+				Zoom:    0.3,
+			},
+		},
+	}
+
+	signedDocURL, err := c.SignDocByKeywordV2(si)
+	if err != nil {
+		t.Fatalf("SignDocByKeywordV2 failed: %v", err)
 	}
 
 	fmt.Println("签名文档路径: ", signedDocURL)
